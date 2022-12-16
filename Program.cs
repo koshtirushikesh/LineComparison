ff0c2923465f081aeff30bcfff5e502cb82dae47
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-16 09:36:25 +0530

    Line Comparison by Cartesian system

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LineComparison
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to line Comparison");
+
+            LengthOfLine.Length_Of_line();
+
+        }
+    }
+}
