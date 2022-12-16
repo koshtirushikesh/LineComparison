644b1064745132b1cf506db1d9c65c299275a604
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-16 09:38:33 +0530

    Line Comparison by Cartesian system

---
+++ LengthOfLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class LengthOfLine
+    {
+        public static void Length_Of_line()
+        {
+            int x1 = 10; int y1 = 20;
+            int x2 = 30; int y2 = 50;
+            
+            double X1 = Math.Pow((x2 - x1), 2); 
+            double Y1 = Math.Pow((y2 - y1), 2); 
+            double length = Math.Sqrt(X1 + Y1);
+            Console.WriteLine(length);
+            
+        }
+    }
+}
